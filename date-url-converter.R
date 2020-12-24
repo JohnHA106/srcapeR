9e0064297df009125ee64c179a8d04d08d14b77c
jobbie <jhunterallan@gmail.com>
2020-12-24 16:10:05 +0000

    trying to make a function that will add a given number in to a specific place in a url

--- date-url-converter.R
+++ date-url-converter.R
@@ -1,1 +1,16 @@
 ##need to make a function that does some maths to convert a date to a doc number
+##for 18th december 2020 the url is https://commonsbusiness.parliament.uk/document/44075/html
+##44075 is the doc number for this
+##nope that's not right
+## each doc uploaded is given a specific number in sequence
+#there are a few added every day
+## so there is no mathematical rule that can predict the number for a specific given date
+#let's start at least by making a function that can add in a number of your choice to the url
+
+urlmaker <- function(number) {
+  return(c("https://commonsbusiness.parliament.uk/document/", number, "/html"))
+}
+
+urlmaker <- Vectorize(urlmaker)
+fakeurl <- urlmaker("1234")
+fakeurl
