420e406a226f8d4f8c46005d798b4e0d794b4552
jobbie <jhunterallan@gmail.com>
2020-12-24 14:14:49 +0000

    updating, not sure if im going down the right path or not

--- scrapinginfo.R
+++ scrapinginfo.R
@@ -7,4 +7,18 @@ dec18oob
 ##this contains the html for order of business in commons on 18th december
 ##will use this as a toy to find out how to extract relevant info
 
+#using developer mode it looks like the info I want is under a div with id = "content goes here" class = "section"
+
+dec18nodes <- dec18oob %>%
+  html_node("body") %>%
+  html_children()
+
+dec18nodes
+#try deeper
+dec18nodes2 <- html_children(dec18nodes)
+#paraMotionText is the class i think I want
+contentdec18 <- html_nodes(dec18oob, "paraMotionText")
+dec18nodes <- html_node(dec18oob, "body")
+dec18nodes <- html_children(dec18nodes)
+dec18meat <- dec18nodes[[5]] #think this contains the <p> nodes I want
 
